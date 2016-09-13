8079a43178de0ab547b1c3b8f2fea16838b85ef9
Joel McCracken <mccracken.joel@gmail.com>
2016-09-13 17:33:11 -0400

    wip

--- lib/funstation_elixir.ex
+++ lib/funstation_elixir.ex
@@ -2,4 +2,12 @@ defmodule FunstationElixir do
   def heartbeat do
     IO.puts "Hello, heart!"
   end
+
+  def heartbeat2 do
+    System.cmd("rclone",
+      ["sync",
+       Path.expand("~/Files"),
+       "s3-personal:my-files"]
+    )
+  end
 end
--- lib/tasks/backup.ex
+++ lib/tasks/backup.ex
@@ -2,10 +2,11 @@ defmodule Mix.Tasks.Backup do
   use Mix.Task
 
   def run(_args) do
-    System.cmd("rclone",
+    {_, status} = System.cmd("rclone",
       ["sync",
       Path.expand("~/Files"),
       "s3-personal:my-files"]
     )
+    IO.puts "Exit status is #{status}"
   end
 end
